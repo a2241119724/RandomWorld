d4006d27d5de8ff2cbc22985057c01fc4a940fe0
liuanbo <2241119724@qq.com>
2025-10-14 21:31:32 +0800

    feat: 天气适应相机缩放

--- Scripts/2D/UI/Panel/PanelUI/ForegroundUI/GameTimeUI.cs
+++ Scripts/2D/UI/Panel/PanelUI/ForegroundUI/GameTimeUI.cs
@@ -35,7 +35,7 @@
             double time = this.curGameTime * this.rate;
 
             // 将sin函数转为周期为1的函数
-            this.globalLight.intensity = Mathf.Clamp(Mathf.Sin(((float)this.curGameTime / GlobalData.GameDayTime * 6.2624f) - 1.55f) + 0.7f, 0.2f, 0.9f);
+            this.globalLight.intensity = Mathf.Clamp(Mathf.Sin(((float)this.curGameTime / GlobalData.GameDayTime * 6.2624f) - 1.55f) + 0.7f, 0.5f, 0.9f);
             this.gameTime.text = string.Format(
                 "<color=blue>游戏时间: </color>{0:D2}天{1:D2}时{2:D2}分",
                 (int)time / DayTime,
