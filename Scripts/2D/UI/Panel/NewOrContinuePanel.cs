92c7e5ae12ca2096dc6199c0456806e71fcb5485
liuanbo <2241119724@qq.com>
2026-05-07 23:20:34 +0800

    refactor(UI): 统一确认面板和重命名面板的命名并移除未使用的代码

--- Scripts/2D/UI/Panel/NewOrContinuePanel.cs
+++ Scripts/2D/UI/Panel/NewOrContinuePanel.cs
@@ -2,7 +2,6 @@ namespace LAB2D
 {
     using System.Collections.Generic;
     using UnityEngine;
-    using UnityEngine.Events;
     using UnityEngine.UI;
 
     /// <summary>
@@ -12,8 +11,6 @@ namespace LAB2D
     {
         private readonly List<Button> archiveSlotButtons = new ();
         private RectTransform content;
-        private Font uiFont;
-        private Sprite buttonSprite;
         private GameObject renamePanel;
         private InputField renameInputField;
         private Text renameTipText;
@@ -27,8 +24,6 @@ namespace LAB2D
             this.Name = "NewOrContinue";
             this.Init();
             this.content = Tool.GetComponentInChildren<RectTransform>(this.Panel, "Content");
-            this.uiFont = this.GetUIFont();
-            this.buttonSprite = Resources.Load<Sprite>("Images/UI/ButtonBackground");
             this.BindArchiveSlotButtons();
             this.CreateRenamePanel();
             this.CreateClearConfirmPanel();
@@ -96,80 +91,6 @@ namespace LAB2D
             return this.FindChildComponent<Button>(gameObject.transform, "Save");
         }
 
-        private GameObject CreateUIObject(string name, Transform parent)
-        {
-            GameObject gameObject = new (name);
-            gameObject.transform.SetParent(parent, false);
-            gameObject.layer = parent.gameObject.layer;
-            gameObject.AddComponent<RectTransform>();
-            return gameObject;
-        }
-
-        private Button CreateButton(string name, Transform parent, string text, Vector2 size, UnityAction onClick)
-        {
-            GameObject gameObject = this.CreateUIObject(name, parent);
-            RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
-            rectTransform.sizeDelta = size;
-
-            Image image = gameObject.AddComponent<Image>();
-            image.sprite = this.buttonSprite;
-            image.type = Image.Type.Simple;
-            image.color = PixelUITheme.ButtonNormal;
-
-            Button button = gameObject.AddComponent<Button>();
-            ColorBlock colors = button.colors;
-            colors.normalColor = PixelUITheme.ButtonNormal;
-            colors.highlightedColor = PixelUITheme.ButtonHighlighted;
-            colors.pressedColor = PixelUITheme.ButtonPressed;
-            colors.selectedColor = PixelUITheme.ButtonSelected;
-            colors.disabledColor = new Color(0.95f, 0.63f, 0.69f, 0.4f);
-            button.colors = colors;
-            button.targetGraphic = image;
-            if (onClick != null)
-            {
-                button.onClick.AddListener(onClick);
-            }
-
-            GameObject textObject = this.CreateText(
-                "Text",
-                gameObject.transform,
-                text,
-                22,
-                TextAnchor.MiddleCenter,
-                Color.white,
-                FontStyle.Bold);
-            RectTransform textRect = textObject.GetComponent<RectTransform>();
-            textRect.anchorMin = Vector2.zero;
-            textRect.anchorMax = Vector2.one;
-            textRect.offsetMin = Vector2.zero;
-            textRect.offsetMax = Vector2.zero;
-            return button;
-        }
-
-        private GameObject CreateText(
-            string name,
-            Transform parent,
-            string textValue,
-            int fontSize,
-            TextAnchor alignment,
-            Color color,
-            FontStyle fontStyle = FontStyle.Normal)
-        {
-            GameObject gameObject = this.CreateUIObject(name, parent);
-            Text text = gameObject.AddComponent<Text>();
-            text.font = this.uiFont;
-            text.fontSize = fontSize;
-            text.fontStyle = fontStyle;
-            text.resizeTextForBestFit = true;
-            text.resizeTextMinSize = 16;
-            text.resizeTextMaxSize = fontSize;
-            text.alignment = alignment;
-            text.color = color;
-            text.text = textValue;
-            text.raycastTarget = false;
-            return gameObject;
-        }
-
         private Transform FindChildTransform(Transform root, string name)
         {
             Transform[] transforms = root.GetComponentsInChildren<Transform>(true);
@@ -205,7 +126,7 @@ namespace LAB2D
 
         private void CreateRenamePanel()
         {
-            Transform panelTransform = this.FindChildTransform(this.Panel.transform, "RenameArchivePanel");
+            Transform panelTransform = this.FindChildTransform(this.Panel.transform, "RenameArchive");
             if (panelTransform != null)
             {
                 this.renamePanel = panelTransform.gameObject;
@@ -229,132 +150,6 @@ namespace LAB2D
                 this.renamePanel.SetActive(false);
                 return;
             }
-
-            this.CreateRuntimeRenamePanel();
-        }
-
-        private void CreateRuntimeRenamePanel()
-        {
-            this.renamePanel = this.CreateUIObject("RenameArchivePanel", this.Panel.transform);
-            RectTransform panelRect = this.renamePanel.GetComponent<RectTransform>();
-            panelRect.anchorMin = Vector2.zero;
-            panelRect.anchorMax = Vector2.one;
-            panelRect.offsetMin = Vector2.zero;
-            panelRect.offsetMax = Vector2.zero;
-
-            Image shade = this.renamePanel.AddComponent<Image>();
-            shade.color = PixelUITheme.DialogShadeDark;
-            shade.raycastTarget = true;
-
-            GameObject box = this.CreateUIObject("Box", this.renamePanel.transform);
-            RectTransform boxRect = box.GetComponent<RectTransform>();
-            boxRect.anchorMin = new Vector2(0.5f, 0.5f);
-            boxRect.anchorMax = new Vector2(0.5f, 0.5f);
-            boxRect.pivot = new Vector2(0.5f, 0.5f);
-            boxRect.anchoredPosition = Vector2.zero;
-            boxRect.sizeDelta = new Vector2(560.0f, 300.0f);
-
-            Image boxImage = box.AddComponent<Image>();
-            boxImage.color = PixelUITheme.DialogBoxBg;
-            boxImage.raycastTarget = true;
-
-            GameObject tip = this.CreateText(
-                "Tip",
-                box.transform,
-                "修改存档名称",
-                32,
-                TextAnchor.MiddleCenter,
-                PixelUITheme.TextPrimary,
-                FontStyle.Bold);
-            RectTransform tipRect = tip.GetComponent<RectTransform>();
-            tipRect.anchorMin = new Vector2(0.5f, 0.5f);
-            tipRect.anchorMax = new Vector2(0.5f, 0.5f);
-            tipRect.pivot = new Vector2(0.5f, 0.5f);
-            tipRect.anchoredPosition = new Vector2(0.0f, 95.0f);
-            tipRect.sizeDelta = new Vector2(500.0f, 60.0f);
-            this.renameTipText = tip.GetComponent<Text>();
-
-            this.renameInputField = this.CreateNameInputField(box.transform);
-
-            Button confirmButton = this.CreateButton(
-                "Confirm",
-                box.transform,
-                "确认",
-                new Vector2(180.0f, 58.0f),
-                this.OnClick_ConfirmRename);
-            RectTransform confirmRect = confirmButton.GetComponent<RectTransform>();
-            confirmRect.anchorMin = new Vector2(0.5f, 0.5f);
-            confirmRect.anchorMax = new Vector2(0.5f, 0.5f);
-            confirmRect.pivot = new Vector2(0.5f, 0.5f);
-            confirmRect.anchoredPosition = new Vector2(-105.0f, -95.0f);
-
-            Button cancelButton = this.CreateButton(
-                "Cancel",
-                box.transform,
-                "取消",
-                new Vector2(180.0f, 58.0f),
-                this.HideRenamePanel);
-            RectTransform cancelRect = cancelButton.GetComponent<RectTransform>();
-            cancelRect.anchorMin = new Vector2(0.5f, 0.5f);
-            cancelRect.anchorMax = new Vector2(0.5f, 0.5f);
-            cancelRect.pivot = new Vector2(0.5f, 0.5f);
-            cancelRect.anchoredPosition = new Vector2(105.0f, -95.0f);
-
-            this.renamePanel.SetActive(false);
-        }
-
-        private InputField CreateNameInputField(Transform parent)
-        {
-            GameObject inputObject = this.CreateUIObject("NameInput", parent);
-            RectTransform inputRect = inputObject.GetComponent<RectTransform>();
-            inputRect.anchorMin = new Vector2(0.5f, 0.5f);
-            inputRect.anchorMax = new Vector2(0.5f, 0.5f);
-            inputRect.pivot = new Vector2(0.5f, 0.5f);
-            inputRect.anchoredPosition = new Vector2(0.0f, 22.0f);
-            inputRect.sizeDelta = new Vector2(440.0f, 58.0f);
-
-            Image inputBackground = inputObject.AddComponent<Image>();
-            inputBackground.color = Color.white;
-            inputBackground.raycastTarget = true;
-
-            InputField inputField = inputObject.AddComponent<InputField>();
-            inputField.characterLimit = ArchiveManager.ArchiveDisplayNameMaxLength;
-            inputField.lineType = InputField.LineType.SingleLine;
-
-            Text text = this.CreateInputFieldText(
-                "Text",
-                inputObject.transform,
-                string.Empty,
-                Color.black,
-                26);
-            text.supportRichText = false;
-            inputField.textComponent = text;
-
-            Text placeholder = this.CreateInputFieldText(
-                "Placeholder",
-                inputObject.transform,
-                "请输入存档名称",
-                new Color(0.55f, 0.55f, 0.55f, 1.0f),
-                24);
-            inputField.placeholder = placeholder;
-            return inputField;
-        }
-
-        private Text CreateInputFieldText(string name, Transform parent, string textValue, Color color, int fontSize)
-        {
-            GameObject textObject = this.CreateText(
-                name,
-                parent,
-                textValue,
-                fontSize,
-                TextAnchor.MiddleLeft,
-                color);
-            RectTransform textRect = textObject.GetComponent<RectTransform>();
-            textRect.anchorMin = Vector2.zero;
-            textRect.anchorMax = Vector2.one;
-            textRect.offsetMin = new Vector2(12.0f, 0.0f);
-            textRect.offsetMax = new Vector2(-12.0f, 0.0f);
-            return textObject.GetComponent<Text>();
         }
 
         private void ShowRenamePanel(int archiveIndex)
@@ -422,7 +217,7 @@ namespace LAB2D
 
         private void CreateClearConfirmPanel()
         {
-            Transform panelTransform = this.FindChildTransform(this.Panel.transform, "ClearConfirmPanel");
+            Transform panelTransform = this.FindChildTransform(this.Panel.transform, "ClearConfirm");
             if (panelTransform != null)
             {
                 this.clearConfirmPanel = panelTransform.gameObject;
@@ -445,76 +240,6 @@ namespace LAB2D
                 this.clearConfirmPanel.SetActive(false);
                 return;
             }
-
-            this.CreateRuntimeClearConfirmPanel();
-        }
-
-        private void CreateRuntimeClearConfirmPanel()
-        {
-            this.clearConfirmPanel = this.CreateUIObject("ClearConfirmPanel", this.Panel.transform);
-            RectTransform panelRect = this.clearConfirmPanel.GetComponent<RectTransform>();
-            panelRect.anchorMin = Vector2.zero;
-            panelRect.anchorMax = Vector2.one;
-            panelRect.offsetMin = Vector2.zero;
-            panelRect.offsetMax = Vector2.zero;
-
-            Image shade = this.clearConfirmPanel.AddComponent<Image>();
-            shade.color = PixelUITheme.DialogShadeDark;
-            shade.raycastTarget = true;
-
-            GameObject box = this.CreateUIObject("Box", this.clearConfirmPanel.transform);
-            RectTransform boxRect = box.GetComponent<RectTransform>();
-            boxRect.anchorMin = new Vector2(0.5f, 0.5f);
-            boxRect.anchorMax = new Vector2(0.5f, 0.5f);
-            boxRect.pivot = new Vector2(0.5f, 0.5f);
-            boxRect.anchoredPosition = Vector2.zero;
-            boxRect.sizeDelta = new Vector2(560.0f, 260.0f);
-
-            Image boxImage = box.AddComponent<Image>();
-            boxImage.color = PixelUITheme.DialogBoxBg;
-            boxImage.raycastTarget = true;
-
-            GameObject tip = this.CreateText(
-                "Tip",
-                box.transform,
-                string.Empty,
-                32,
-                TextAnchor.MiddleCenter,
-                PixelUITheme.TextPrimary,
-                FontStyle.Bold);
-            RectTransform tipRect = tip.GetComponent<RectTransform>();
-            tipRect.anchorMin = new Vector2(0.5f, 0.5f);
-            tipRect.anchorMax = new Vector2(0.5f, 0.5f);
-            tipRect.pivot = new Vector2(0.5f, 0.5f);
-            tipRect.anchoredPosition = new Vector2(0.0f, 55.0f);
-            tipRect.sizeDelta = new Vector2(500.0f, 110.0f);
-            this.clearConfirmText = tip.GetComponent<Text>();
-
-            Button confirmButton = this.CreateButton(
-                "Confirm",
-                box.transform,
-                "确认清除",
-                new Vector2(190.0f, 58.0f),
-                this.OnClick_ConfirmClear);
-            RectTransform confirmRect = confirmButton.GetComponent<RectTransform>();
-            confirmRect.anchorMin = new Vector2(0.5f, 0.5f);
-            confirmRect.anchorMax = new Vector2(0.5f, 0.5f);
-            confirmRect.pivot = new Vector2(0.5f, 0.5f);
-            confirmRect.anchoredPosition = new Vector2(-110.0f, -70.0f);
-
-            Button cancelButton = this.CreateButton(
-                "Cancel",
-                box.transform,
-                "取消",
-                new Vector2(190.0f, 58.0f),
-                this.HideClearConfirmPanel);
-            RectTransform cancelRect = cancelButton.GetComponent<RectTransform>();
-            cancelRect.anchorMin = new Vector2(0.5f, 0.5f);
-            cancelRect.anchorMax = new Vector2(0.5f, 0.5f);
-            cancelRect.pivot = new Vector2(0.5f, 0.5f);
-            cancelRect.anchoredPosition = new Vector2(110.0f, -70.0f);
-
-            this.clearConfirmPanel.SetActive(false);
         }
 
         private void ShowClearConfirmPanel(int archiveIndex)
@@ -595,21 +320,6 @@ namespace LAB2D
             GlobalData.IsNew = false;
             this.Controller.Show(AsyncProgressPanel.Instance);
             ArchiveManager.Instance.LoadCurrentArchive();
-        }
-
-        private Font GetUIFont()
-        {
-            Text[] texts = this.Panel.GetComponentsInChildren<Text>(true);
-            foreach (Text text in texts)
-            {
-                if (text.font != null)
-                {
-                    return text.font;
-                }
-            }
-
-            Font font = Resources.Load<Font>("Font/ark-pixel-12px-monospaced-zh_cn");
-            return font != null ? font : Resources.GetBuiltinResource<Font>("Arial.ttf");
         }
     }
 }
