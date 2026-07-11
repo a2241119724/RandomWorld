7e14071884fec50ee1a522194600544b389e041b
liuanbo <2241119724@qq.com>
2026-07-12 01:11:24 +0800

    refactor add missing operators and IEquatable to GameVector2 and GameGridPosition

--- Scripts/2D/Domain/Common/GameGridPosition.cs
+++ Scripts/2D/Domain/Common/GameGridPosition.cs
@@ -1,10 +1,12 @@
 namespace LAB2D
 {
+    using System;
+
     /// <summary>
     /// Engine-agnostic grid position value object.
     /// Replaces UnityEngine.Vector3Int in pure domain code.
     /// </summary>
-    public readonly struct GameGridPosition
+    public readonly struct GameGridPosition : IEquatable<GameGridPosition>
     {
         public readonly int X;
         public readonly int Y;
@@ -22,17 +24,29 @@ namespace LAB2D
             return new GameGridPosition(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
         }
 
-        public override string ToString()
+        public static GameGridPosition operator -(GameGridPosition a, GameGridPosition b)
         {
-            return $"({this.X}, {this.Y}, {this.Z})";
+            return new GameGridPosition(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        public static bool operator ==(GameGridPosition a, GameGridPosition b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+
+        public static bool operator !=(GameGridPosition a, GameGridPosition b)
+        {
+            return !(a == b);
         }
 
         public override bool Equals(object obj)
         {
-            return obj is GameGridPosition other &&
-                this.X == other.X &&
-                this.Y == other.Y &&
-                this.Z == other.Z;
+            return obj is GameGridPosition other && this == other;
+        }
+
+        public bool Equals(GameGridPosition other)
+        {
+            return this == other;
         }
 
         public override int GetHashCode()
@@ -42,6 +56,11 @@ namespace LAB2D
             hash = (hash * 31) + this.Y;
             hash = (hash * 31) + this.Z;
             return hash;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.X}, {this.Y}, {this.Z})";
         }
     }
 }
--- Scripts/2D/Domain/Common/GameVector2.cs
+++ Scripts/2D/Domain/Common/GameVector2.cs
@@ -1,9 +1,11 @@
 namespace LAB2D
 {
+    using System;
+
     /// <summary>
     /// Engine-agnostic 2D vector used by pure gameplay rules.
     /// </summary>
-    public readonly struct GameVector2
+    public readonly struct GameVector2 : IEquatable<GameVector2>
     {
         public readonly float X;
         public readonly float Y;
@@ -19,6 +21,59 @@ namespace LAB2D
             float dx = this.X - other.X;
             float dy = this.Y - other.Y;
             return (dx * dx) + (dy * dy);
+        }
+
+        public static GameVector2 operator +(GameVector2 a, GameVector2 b)
+        {
+            return new GameVector2(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static GameVector2 operator -(GameVector2 a, GameVector2 b)
+        {
+            return new GameVector2(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static GameVector2 operator *(GameVector2 v, float scalar)
+        {
+            return new GameVector2(v.X * scalar, v.Y * scalar);
+        }
+
+        public static GameVector2 operator *(float scalar, GameVector2 v)
+        {
+            return new GameVector2(v.X * scalar, v.Y * scalar);
+        }
+
+        public static bool operator ==(GameVector2 a, GameVector2 b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool operator !=(GameVector2 a, GameVector2 b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is GameVector2 other && this == other;
+        }
+
+        public bool Equals(GameVector2 other)
+        {
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = (hash * 31) + this.X.GetHashCode();
+            hash = (hash * 31) + this.Y.GetHashCode();
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.X:F2}, {this.Y:F2})";
         }
     }
 }
