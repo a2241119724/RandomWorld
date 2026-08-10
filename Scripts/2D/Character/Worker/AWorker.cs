c9d61d03400d8d529666e2a68cd8875ac06c39ed
liuanbo <2241119724@qq.com>
2026-08-10 13:07:37 +0800

    fix(worker): 将工人角色日志等级从info改为trace
    调整了AWorker脚本中的初始化日志输出等级，将info级别改为trace，避免无关info日志过多

--- Scripts/2D/Character/Worker/AWorker.cs
+++ Scripts/2D/Character/Worker/AWorker.cs
@@ -129,7 +129,7 @@ namespace LAB2D.Character.Worker
                     if (this.dialogText != null)
                     {
                         this.dialogRoot.SetActive(false);
-                        LogProvider($"[Monologue] {this.name} Dialog/Text 初始化成功", LogManager.LogLevelEnum.Info);
+                        LogProvider($"[Monologue] {this.name} Dialog/Text 初始化成功", LogManager.LogLevelEnum.Trace);
                     }
                     else
                     {
